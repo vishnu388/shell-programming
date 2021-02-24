e1e2e800dd7b04a0aa4d39d2cb467b039d83ca4b
vishnu <vishnu388@gmail.com>
2021-02-24 15:51:48 +0530

    Added: to show the 2n value

---
+++ while-loop-statement/power.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+read -p "enter the nth number:" number
+
+counter=0
+result=0
+while (($counter<= $number && $result < 256))
+do
+    result=$((2**counter))
+    echo $result
+    counter=$((counter+1))
+done
+
