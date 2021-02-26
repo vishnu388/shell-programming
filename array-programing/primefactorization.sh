1ad6493253d203cec276261c445ae5ff670b701c
vishnu <vishnu388@gmail.com>
2021-02-26 17:06:35 +0530

    Added: to find the primefactarization method

---
+++ array-programing/primefactorization.sh
@@ -0,0 +1,20 @@
+#! /bin/bash 
+
+read -p "Enter the number to find primefactors:" number
+
+declare a primeFactors
+counter=0
+primeFactorsOf=$number
+
+for ((count=2 ; count<= $number ; count++ ))
+do
+	while (($number%$count == 0 ))
+	do
+		primeFactors[((counter++))]=$count
+		number=$((number/count))
+	done
+done
+
+echo "Prime factors of $primeFactorsOf is ${primeFactors[@]}"
+
+
