4be8f29ffb1fa195c9104a81da9196c963272751
vishnu <vishnu388@gmail.com>
2021-02-27 12:28:10 +0530

    Added: employee is present or absent

---
+++ empattendance.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+isPresent=1
+
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ];
+then
+     echo "Employee is present";
+else
+     echo "Employee is absent";
+fi
+
